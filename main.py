c1685ac91f2158a72b22347f2bd0f8d0aa662b67
ThisProgrammerG <116992225+ThisProgrammerG@users.noreply.github.com>
2023-03-31 01:54:51 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+assets_path = Path().cwd()
+
+# Parameters hardcoded for intellisense.
+def get_path(*, fonts: str = '', images: str = '', musics: str = '', sounds: str = ''):
+    directory_name, file_name = next(((k, v) for k, v in locals().items() if v), (None, None))
+    if not file_name:
+        raise ValueError(f'Missing file name.')
+    return assets_path / directory_name / file_name
+
+print(get_path(fonts='supertastic.ttf'))
+print(get_path(images='field_of_stars.png'))
+print(get_path(musics='boss_fight.wav'))
+print(get_path(sounds='bubble_pop.ogg'))
+print(get_path(images='wolves/big_bad_wolf.png'))
